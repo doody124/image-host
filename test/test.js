e6c38073742e940a5f9cf8a0c2b885781142b83e
doody124 <123220389+doody124@users.noreply.github.com>
2023-12-20 02:39:18 -0600

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-var quotes = ["quote 1", "quote 2", "quote 3"];
+var quotes = ["quote 1", "quote 2", "quote 31"];
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * quotes.length);
